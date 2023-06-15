aa8c1b297609302b3a01668b94b71af49d8b8c5d
kaleneDev <apiou.kalene@gmail.com>
2023-06-15 15:13:20 +0200

    delete background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import "./style/App.scss";
-// import Hero from "./components/Hero/Hero";
-// import Programs from "./components/Programs/Programs";
-// import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
-// import Testimonials from "./components/Testimonials/Testimonials";
-// import Plans from "./components/Plans/Plans";
-// import Join from "./components/Join/Join";
+import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
+import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Plans from "./components/Plans/Plans";
+import Join from "./components/Join/Join";
 import Footer from "./components/Footer/Footer";
 require("./data/fontawesome");
 
 function App() {
     return (
         <div className="App">
-            {/* <Hero></Hero> */}
-            {/* <Programs></Programs> */}
-            {/* <WhyChooseUs></WhyChooseUs> */}
-            {/* <Plans></Plans> */}
-            {/* <Testimonials></Testimonials> */}
-            {/* <Join></Join> */}
+            <Hero></Hero>
+            <Programs></Programs>
+            <WhyChooseUs></WhyChooseUs>
+            <Plans></Plans>
+            <Testimonials></Testimonials>
+            <Join></Join>
             <Footer></Footer>
         </div>
     );
